59712ccf78aa66429b247e3b7c0b64dad5642502
john-chang94 <j.chang94@yahoo.com>
2020-12-18 16:10:25 -0800

    added filtering for get restaurants

--- routes/dashRoutes.js
+++ routes/dashRoutes.js
@@ -1,15 +1,43 @@
 const router = require('express').Router();
 // const pool = require('../db');
-const client = require('../elephantsql');
+const client = require('../config/db');
 const { addRestaurantValidator } = require('../middlewares/validator');
+
+const queryCheck = async (reqQuery) => {
+    let isQuery = false;
+    let values = [];
+    let num = 1;
+    let querystr = ['SELECT * FROM restaurants WHERE'];
+    const validQueries = ['name', 'city', 'category', 'price_range'];
+
+    for (let i = 0; i < validQueries.length; i++) {
+        // Check if there are any valid query params in req.query
+        if (reqQuery.hasOwnProperty(validQueries[i])) {
+            // Append SQL command with query param
+            querystr.push(`${validQueries[i]} = $${num++}`);
+            // Also include SQL command 'AND' for multiple query params
+            querystr.push('AND');
+            // Add the value of the query param in the values array
+            values.push(reqQuery[validQueries[i]]);
+            isQuery = true;
+        }
+    }
+    // Remove the extra 'AND' at the end of the querystr array
+    querystr.pop()
+    // Join the querystr array to get one whole query string
+    let text = querystr.join(' ');
+    // Default query string
+    if (!isQuery) text = 'SELECT * FROM restaurants'
+
+    let results = await client.query(text, values);
+    return results;
+}
 
 router.get('/restaurants', async (req, res) => {
     try {
-        const results = await client.query(
-            `SELECT * FROM restaurants`
-        )
+        const results = await queryCheck(req.query);
 
-        if (results.rows.length === 0) {
+        if (!results.rows.length) {
             return res.status(404).send('No restaurants');
         }
 
@@ -31,13 +59,13 @@ router.get('/restaurants/:id', async (req, res) => {
             [id]
         )
 
-        if (results.rows.length === 0) {
+        if (!results.rows.length) {
             return res.status(404).send('No restaurant found');
         }
 
         res.status(200).json({
             status: 'success',
-            results: results.length,
+            results: results.rows.length,
             data: results.rows[0]
         })
     } catch (err) {
@@ -98,3 +126,8 @@ router.delete('/restaurants/:id', async (req, res) => {
 })
 
 module.exports = router;
+
+// console.log(req.query)
+// console.log(Object.keys(req.query))
+// console.log(req.query.hasOwnProperty('city'))
+// console.log(req.query['city'])
